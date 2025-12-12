75cf282be27d74d8e8878c7e630a39e91f2aef1e
maria <valentinaaldama15@gmail.com>
2025-12-12 16:06:28 -0500

    Fix classNam error and update button state

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -2,11 +2,16 @@ import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 import { useDispatch } from "react-redux";
-import { addToCart } from "./CartSlice";
+import { addItem}from "./CartSlice";
+import { useSelector } from "react-redux";
+
+
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false);// State to control the visibility of the About Us page
     const dispatch = useDispatch();
+
+    const items = useSelector((state) => state.cart.items);
 
     const plantsArray = [
         {
@@ -17,42 +22,48 @@ function ProductList({ onHomeClick }) {
                     name: "Snake Plant",
                     image: "https://cdn.pixabay.com/photo/2021/01/22/06/04/snake-plant-5939187_1280.jpg",
                     description: "Produces oxygen at night, improving air quality.",
-                    price: 15
+                    price: 15,
+                    added:false, 
                 },
                 {
                     id:2,
                     name: "Spider Plant",
                     image: "https://cdn.pixabay.com/photo/2018/07/11/06/47/chlorophytum-3530413_1280.jpg",
                     description: "Filters formaldehyde and xylene from the air.",
-                    price: 12
+                    price: 12,
+                    added:false,
                 },
                 {
                     id:3,
                     name: "Peace Lily",
                     image: "https://cdn.pixabay.com/photo/2019/06/12/14/14/peace-lilies-4269365_1280.jpg",
                     description: "Removes mold spores and purifies the air.",
-                    price: 18
+                    price: 18,
+                    added:false,
                 },
                 {
                     id:4,
                     name: "Boston Fern",
                     image: "https://cdn.pixabay.com/photo/2020/04/30/19/52/boston-fern-5114414_1280.jpg",
                     description: "Adds humidity to the air and removes toxins.",
-                    price: 20
+                    price: 20,
+                    added:false,
                 },
                 {
                     id:5,
                     name: "Rubber Plant",
                     image: "https://cdn.pixabay.com/photo/2020/02/15/11/49/flower-4850729_1280.jpg",
                     description: "Easy to care for and effective at removing toxins.",
-                    price: 17
+                    price: 17,
+                    added:false,
                 },
                 {
                     id:6,
                     name: "Aloe Vera",
                     image: "https://cdn.pixabay.com/photo/2018/04/02/07/42/leaf-3283175_1280.jpg",
                     description: "Purifies the air and has healing properties for skin.",
-                    price: 14
+                    price: 14,
+                    added:false,
                 }
             ]
         },
@@ -64,42 +75,48 @@ function ProductList({ onHomeClick }) {
                     name: "Lavender",
                     image: "https://images.unsplash.com/photo-1611909023032-2d6b3134ecba?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
                     description: "Calming scent, used in aromatherapy.",
-                    price: 20
+                    price: 20,
+                    added:false,
                 },
                 {
                     id:8,
                     name: "Jasmine",
                     image: "https://images.unsplash.com/photo-1592729645009-b96d1e63d14b?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
                     description: "Sweet fragrance, promotes relaxation.",
-                    price: 18
+                    price: 18,
+                    added:false,
                 },
                 {
                     id:9,
                     name: "Rosemary",
                     image: "https://cdn.pixabay.com/photo/2019/10/11/07/12/rosemary-4541241_1280.jpg",
                     description: "Invigorating scent, often used in cooking.",
-                    price: 15
+                    price: 15,
+                    added:false,
                 },
                 {
                     id:10,
                     name: "Mint",
                     image: "https://cdn.pixabay.com/photo/2016/01/07/18/16/mint-1126282_1280.jpg",
                     description: "Refreshing aroma, used in teas and cooking.",
-                    price: 12
+                    price: 12,
+                    added:false,
                 },
                 {
                     id:11,
                     name: "Lemon Balm",
                     image: "https://cdn.pixabay.com/photo/2019/09/16/07/41/balm-4480134_1280.jpg",
                     description: "Citrusy scent, relieves stress and promotes sleep.",
-                    price: 14
+                    price: 14,
+                    added:false,
                 },
                 {
                     id:12,
                     name: "Hyacinth",
                     image: "https://cdn.pixabay.com/photo/2019/04/07/20/20/hyacinth-4110726_1280.jpg",
                     description: "Hyacinth is a beautiful flowering plant known for its fragrant.",
-                    price: 22
+                    price: 22,
+                    added:false,
                 }
             ]
         },
@@ -111,42 +128,48 @@ function ProductList({ onHomeClick }) {
                     name: "oregano",
                     image: "https://cdn.pixabay.com/photo/2015/05/30/21/20/oregano-790702_1280.jpg",
                     description: "The oregano plants contains compounds that can deter certain insects.",
-                    price: 10
+                    price: 10,
+                    added:false,
                 },
                 {
                     id:14,
                     name: "Marigold",
                     image: "https://cdn.pixabay.com/photo/2022/02/22/05/45/marigold-7028063_1280.jpg",
                     description: "Natural insect repellent, also adds color to the garden.",
-                    price: 8
+                    price: 8,
+                    added:false,
                 },
                 {
                     id:15,
                     name: "Geraniums",
                     image: "https://cdn.pixabay.com/photo/2012/04/26/21/51/flowerpot-43270_1280.jpg",
                     description: "Known for their insect-repelling properties while adding a pleasant scent.",
-                    price: 20
+                    price: 20,
+                    added:false,
                 },
                 {
                     id:16,
                     name: "Basil",
                     image: "https://cdn.pixabay.com/photo/2016/07/24/20/48/tulsi-1539181_1280.jpg",
                     description: "Repels flies and mosquitoes, also used in cooking.",
-                    price: 9
+                    price: 9,
+                    added:false,
                 },
                 {
                     id:17,
                     name: "Lavender",
                     image: "https://images.unsplash.com/photo-1611909023032-2d6b3134ecba?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
                     description: "Calming scent, used in aromatherapy.",
-                    price: 20
+                    price: 20,
+                    added:false,
                 },
                 {
                     id:18,
                     name: "Catnip",
                     image: "https://cdn.pixabay.com/photo/2015/07/02/21/55/cat-829681_1280.jpg",
                     description: "Repels mosquitoes and attracts cats.",
-                    price: 13
+                    price: 13,
+                    added:false,
                 }
             ]
         },
@@ -158,42 +181,48 @@ function ProductList({ onHomeClick }) {
                     name: "Aloe Vera",
                     image: "https://cdn.pixabay.com/photo/2018/04/02/07/42/leaf-3283175_1280.jpg",
                     description: "Soothing gel used for skin ailments.",
-                    price: 14
+                    price: 14,
+                    added:false,
                 },
                 {
                     id:20,
                     name: "Echinacea",
                     image: "https://cdn.pixabay.com/photo/2014/12/05/03/53/echinacea-557477_1280.jpg",
                     description: "Boosts immune system, helps fight colds.",
-                    price: 16
+                    price: 16,
+                    added:false,
                 },
                 {
                     id:21,
                     name: "Peppermint",
                     image: "https://cdn.pixabay.com/photo/2017/07/12/12/23/peppermint-2496773_1280.jpg",
                     description: "Relieves digestive issues and headaches.",
-                    price: 13
+                    price: 13,
+                    added:false,
                 },
                 {
                     id:22,
                     name: "Lemon Balm",
                     image: "https://cdn.pixabay.com/photo/2019/09/16/07/41/balm-4480134_1280.jpg",
                     description: "Calms nerves and promotes relaxation.",
-                    price: 14
+                    price: 14,
+                    added:false,
                 },
                 {
                     id:23,
                     name: "Chamomile",
                     image: "https://cdn.pixabay.com/photo/2016/08/19/19/48/flowers-1606041_1280.jpg",
                     description: "Soothes anxiety and promotes sleep.",
-                    price: 15
+                    price: 15,
+                    added:false,
                 },
                 {
                     id:24,
                     name: "Calendula",
                     image: "https://cdn.pixabay.com/photo/2019/07/15/18/28/flowers-4340127_1280.jpg",
                     description: "Heals wounds and soothes skin irritations.",
-                    price: 12
+                    price: 12,
+                    added:false,
                 }
             ]
         },
@@ -205,42 +234,48 @@ function ProductList({ onHomeClick }) {
                     name: "ZZ Plant",
                     image: "https://images.unsplash.com/photo-1632207691143-643e2a9a9361?q=80&w=464&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
                     description: "Thrives in low light and requires minimal watering.",
-                    price: 25
+                    price: 25,
+                    added:false,
                 },
                 {
                     id:26,
                     name: "Pothos",
                     image: "https://cdn.pixabay.com/photo/2018/11/15/10/32/plants-3816945_1280.jpg",
                     description: "Tolerates neglect and can grow in various conditions.",
-                    price: 10
+                    price: 10,
+                    added:false,
                 },
                 {
                     id:27,
                     name: "Snake Plant",
                     image: "https://cdn.pixabay.com/photo/2021/01/22/06/04/snake-plant-5939187_1280.jpg",
                     description: "Needs infrequent watering and is resilient to most pests.",
-                    price: 15
+                    price: 15,
+                    added:false,
                 },
                 {
                     id:28,
                     name: "Cast Iron Plant",
                     image: "https://cdn.pixabay.com/photo/2017/02/16/18/04/cast-iron-plant-2072008_1280.jpg",
                     description: "Hardy plant that tolerates low light and neglect.",
-                    price: 20
+                    price: 20,
+                    added:false,
                 },
                 {
                     id:29,
                     name: "Succulents",
                     image: "https://cdn.pixabay.com/photo/2016/11/21/16/05/cacti-1846147_1280.jpg",
                     description: "Drought-tolerant plants with unique shapes and colors.",
-                    price: 18
+                    price: 18,
+                    added:false,
                 },
                 {
                     id:30,
                     name: "Aglaonema",
                     image: "https://cdn.pixabay.com/photo/2014/10/10/04/27/aglaonema-482915_1280.jpg",
                     description: "Requires minimal care and adds color to indoor spaces.",
-                    price: 22
+                    price: 22,
+                    added:false,
                 }
             ]
         }
@@ -291,6 +326,10 @@ function ProductList({ onHomeClick }) {
     };
 
 
+   
+
+
+
     return (
         <div>
             <div className="navbar" style={styleObj}>
@@ -320,18 +359,19 @@ function ProductList({ onHomeClick }) {
 
                             <div className="product-list">
                                 {category.plants.map((plant, idx) => (
+
                                     <div key={idx} className="product-card">
                                         <img className="product-image" src={plant.image} alt={plant.name} />
                                         <h3 className="product-title">{plant.name}</h3>
                                         <p className="desc">{plant.description}</p>
                                         <p className="product-price">${plant.price}</p>
 
-                        
-                                        <button 
-                                        className="product-button"  
-                                        onClick={() => handleAddToCart(plant)}
+                                        <button
+                                        onClick={() => dispatch(addItem(plant))}
+                                         disabled={isInCart}
+                                          className={isInCart ? "in-cart" : ""}
                                         >
-                                            Agregar al carrito
+                                            {isInCart ? "Añadido" : "Agregar al carrito"}
                                         </button>
                                     </div>
                                 ))}
