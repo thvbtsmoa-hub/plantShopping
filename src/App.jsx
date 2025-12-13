e8676a353a02bfce95e00eb119ff4ad850c3a27b
maria <valentinaaldama15@gmail.com>
2025-12-13 12:05:36 -0500

    el header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import React, { useState } from 'react';
-import  './ProductList';
+import ProductList from './ProductList';
 import './App.css';
 import AboutUs from './AboutUs';
 import {Link} from 'react-router-dom';
