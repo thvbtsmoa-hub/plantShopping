94dff05eac980687900b7ce6cecbd5514f732f5d
maria <valentinaaldama15@gmail.com>
2025-12-13 12:01:04 -0500

    arreglando appjsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import React, { useState } from 'react';
-import ProductList from './ProductList';
+import  './ProductList';
 import './App.css';
 import AboutUs from './AboutUs';
 import {Link} from 'react-router-dom';
