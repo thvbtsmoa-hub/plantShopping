1fd5930866f94f5f1b79b9c1122d3d0c21915f4a
maria <valentinaaldama15@gmail.com>
2025-12-13 10:08:25 -0500

    nuevo

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,24 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store.js'
+import CartItem from './CartItem';
+import NotFound from './NotFound.jsx'
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import ProductList from './ProductList';
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <BrowserRouter>
+      
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/cart" element={<CartItem />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 )
